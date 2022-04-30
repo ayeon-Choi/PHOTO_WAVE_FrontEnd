ccfb952b9477ee922fca36db54880af65a65485a
ayeonChoi <201120cay@dsm.hs.kr>
2022-05-01 01:29:50 +0900

    :hammer: ::로그인 및 회원가입 페이지 라우팅

--- src/routes/MainRouter.tsx
+++ src/routes/MainRouter.tsx
@@ -1,11 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from '../pages/Main';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "../pages/Login";
+import Main from "../pages/Main";
+import Signup from "../pages/Signup";
 
 const MainRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </BrowserRouter>
   );
